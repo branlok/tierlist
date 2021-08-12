e3a387ee668a6ed1a86c86ccdf5a88b3ea78a912
Brandon Lok <brandonlok@Brandons-MacBook-Pro.local>
2021-08-12 16:45:16 -0400

    .

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ import Dexie from "dexie";
 const db = new Dexie("ReactReduxSampleDB");
 
 // db.version(1).stores({ images: "++id, tierlistId, dateAdded", tierlists: "++id, status, date", items: "++id, name, resides, tierlistId, [tierlistId+resides], [tierlistId+name]"});
-db.version(1).stores({
+db.version(2).stores({
   images: "++id, tierlistId, dateAdded, fileName",
   tierlists: "++id, status, date, title, lastEdited",
   items:
